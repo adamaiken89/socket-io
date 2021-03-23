c222b2400bcdb1f28dbf5eb5fb32e3967e8f8a06
Adam Wan <adamaikenadam@gmail.com>
2021-03-23 10:00:19 +0800

    UPT: example

---
+++ server.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/test', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
